ba0f362a1c3d41ad80d15db23d01aca320c30fe1
Mohamedwaelfaro <100936115+Mohamedwaelfaro@users.noreply.github.com>
2024-07-10 04:52:18 +0300

    fix ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,180 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Task1());
+}
+
+class Task1 extends StatefulWidget {
+  const Task1({super.key});
+
+  @override
+  State<Task1> createState() => _Task1State();
+}
+
+class _Task1State extends State<Task1> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        // appBar: AppBar(
+        //   leading: Icon(Icons.arrow_back_sharp),
+        //   actions: [Icon(Icons.favorite_sharp)],
+        // ),
+
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          leading: Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ),
+          actions: [
+            Icon(
+              Icons.favorite_border,
+              color: Colors.black,
+            ),
+          ],
+        ),
+
+        body: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Image.asset(
+                  'imgs/sofa.png',
+                  height: 300,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
 // import 'package:flutter/material.dart';
 
 // void main() {
-//   runApp(Task1());
+//   runApp(test());
 // }
 
-// class Task1 extends StatefulWidget {
-//   const Task1({super.key});
+// class test extends StatefulWidget {
+//   const test({super.key});
 
 //   @override
-//   State<Task1> createState() => _Task1State();
+//   State<test> createState() => _testState();
 // }
 
-// class _Task1State extends State<Task1> {
+// class _testState extends State<test> {
 //   @override
 //   Widget build(BuildContext context) {
 //     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
 //       home: Scaffold(
-//         // appBar: AppBar(
-//         //   leading: Icon(Icons.arrow_back_sharp),
-//         //   actions: [Icon(Icons.favorite_sharp)],
-//         // ),
-
 //         appBar: AppBar(
+//           title: Text('Sofa Shop'),
 //           backgroundColor: Colors.white,
-//           leading: Icon(
-//             Icons.arrow_back,
-//             color: Colors.black,
+//           leading: IconButton(
+//             icon: Icon(Icons.arrow_back, color: Colors.black),
+//             onPressed: () {},
 //           ),
 //           actions: [
-//             Icon(
-//               Icons.favorite_border,
-//               color: Colors.black,
+//             IconButton(
+//               icon: Icon(Icons.favorite_border, color: Colors.black),
+//               onPressed: () {},
 //             ),
 //           ],
 //         ),
-
 //         body: Padding(
-//           padding: const EdgeInsets.all(16),
+//           padding: const EdgeInsets.all(16.0),
 //           child: Column(
 //             crossAxisAlignment: CrossAxisAlignment.start,
 //             children: [
 //               Center(
 //                 child: Image.asset(
 //                   'imgs/sofa.png',
-//                   height: 300,
+//                   height: 200,
 //                 ),
-//               )
+//               ),
+//               SizedBox(height: 16),
+//               Text(
+//                 'Room Sofa',
+//                 style: TextStyle(
+//                   fontSize: 24,
+//                   fontWeight: FontWeight.bold,
+//                 ),
+//               ),
+//               SizedBox(height: 8),
+//               Row(
+//                 children: [
+//                   CircleAvatar(
+//                     backgroundColor: Colors.blue,
+//                     radius: 8,
+//                   ),
+//                   SizedBox(width: 8),
+//                   CircleAvatar(
+//                     backgroundColor: Colors.brown,
+//                     radius: 8,
+//                   ),
+//                   SizedBox(width: 8),
+//                   CircleAvatar(
+//                     backgroundColor: Colors.black,
+//                     radius: 8,
+//                   ),
+//                 ],
+//               ),
+//               SizedBox(height: 16),
+//               Text(
+//                 'Drawing Room Wooden Sofa Set is solid wooden sofa set which you can contrast the cushion of any color. The good sight caused use to the furniture help us relax for a longer time.',
+//                 style: TextStyle(
+//                   fontSize: 16,
+//                   color: Colors.grey[700],
+//                 ),
+//               ),
+//               SizedBox(height: 16),
+//               Row(
+//                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                 children: [
+//                   Row(
+//                     children: [
+//                       IconButton(
+//                         icon: Icon(Icons.remove),
+//                         onPressed: () {},
+//                       ),
+//                       Text(
+//                         '1',
+//                         style: TextStyle(fontSize: 18),
+//                       ),
+//                       IconButton(
+//                         icon: Icon(Icons.add),
+//                         onPressed: () {},
+//                       ),
+//                     ],
+//                   ),
+//                 ],
+//               ),
+//               Text(
+//                 '¥2500',
+//                 style: TextStyle(
+//                   fontSize: 24,
+//                   fontWeight: FontWeight.bold,
+//                 ),
+//               ),
+//               SizedBox(height: 16),
+//               Center(
+//                 child: ElevatedButton(
+//                   onPressed: () {},
+//                   child: Text('Add to cart'),
+//                   style: ElevatedButton.styleFrom(
+//                     iconColor: Colors.blue,
+//                     padding: EdgeInsets.symmetric(horizontal: 80, vertical: 16),
+//                     textStyle: TextStyle(
+//                       fontSize: 18,
+//                     ),
+//                   ),
+//                 ),
+//               ),
 //             ],
 //           ),
 //         ),
@@ -54,247 +182,3 @@
 //     );
 //   }
 // }
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(MyApp());
-}
-
-class test extends StatefulWidget {
-  const test({super.key});
-
-  @override
-  State<test> createState() => _testState();
-}
-
-class _testState extends State<test> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Sofa Shop'),
-          backgroundColor: Colors.white,
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back, color: Colors.black),
-            onPressed: () {},
-          ),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.favorite_border, color: Colors.black),
-              onPressed: () {},
-            ),
-          ],
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Center(
-                child: Image.asset(
-                  'imgs/sofa.png', // Replace with your image URL
-                  height: 200,
-                ),
-              ),
-              SizedBox(height: 16),
-              Text(
-                'Room Sofa',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 8),
-              Row(
-                children: [
-                  CircleAvatar(
-                    backgroundColor: Colors.blue,
-                    radius: 8,
-                  ),
-                  SizedBox(width: 8),
-                  CircleAvatar(
-                    backgroundColor: Colors.brown,
-                    radius: 8,
-                  ),
-                  SizedBox(width: 8),
-                  CircleAvatar(
-                    backgroundColor: Colors.black,
-                    radius: 8,
-                  ),
-                ],
-              ),
-              SizedBox(height: 16),
-              Text(
-                'Drawing Room Wooden Sofa Set is solid wooden sofa set which you can contrast the cushion of any color. The good sight caused use to the furniture help us relax for a longer time.',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.grey[700],
-                ),
-              ),
-              SizedBox(height: 16),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      IconButton(
-                        icon: Icon(Icons.remove),
-                        onPressed: () {},
-                      ),
-                      Text(
-                        '1',
-                        style: TextStyle(fontSize: 18),
-                      ),
-                      IconButton(
-                        icon: Icon(Icons.add),
-                        onPressed: () {},
-                      ),
-                    ],
-                  ),
-                  Text(
-                    '¥2500',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 16),
-              Center(
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: Text('Add to cart'),
-                  style: ElevatedButton.styleFrom(
-                    iconColor: Colors.blue,
-                    padding: EdgeInsets.symmetric(horizontal: 80, vertical: 16),
-                    textStyle: TextStyle(
-                      fontSize: 18,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Sofa Shop'),
-          backgroundColor: Colors.white,
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back, color: Colors.black),
-            onPressed: () {},
-          ),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.favorite_border, color: Colors.black),
-              onPressed: () {},
-            ),
-          ],
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Center(
-                child: Image.asset(
-                  'imgs/sofa.png', // Replace with your image URL
-                  height: 200,
-                ),
-              ),
-              SizedBox(height: 16),
-              Text(
-                'Room Sofa',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 8),
-              Row(
-                children: [
-                  CircleAvatar(
-                    backgroundColor: Colors.blue,
-                    radius: 8,
-                  ),
-                  SizedBox(width: 8),
-                  CircleAvatar(
-                    backgroundColor: Colors.brown,
-                    radius: 8,
-                  ),
-                  SizedBox(width: 8),
-                  CircleAvatar(
-                    backgroundColor: Colors.black,
-                    radius: 8,
-                  ),
-                ],
-              ),
-              SizedBox(height: 16),
-              Text(
-                'Drawing Room Wooden Sofa Set is solid wooden sofa set which you can contrast the cushion of any color. The good sight caused use to the furniture help us relax for a longer time.',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.grey[700],
-                ),
-              ),
-              SizedBox(height: 16),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      IconButton(
-                        icon: Icon(Icons.remove),
-                        onPressed: () {},
-                      ),
-                      Text(
-                        '1',
-                        style: TextStyle(fontSize: 18),
-                      ),
-                      IconButton(
-                        icon: Icon(Icons.add),
-                        onPressed: () {},
-                      ),
-                    ],
-                  ),
-                  Text(
-                    '¥2500',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 16),
-              Center(
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: Text('Add to cart'),
-                  style: ElevatedButton.styleFrom(
-                    iconColor: Colors.blue,
-                    padding: EdgeInsets.symmetric(horizontal: 80, vertical: 16),
-                    textStyle: TextStyle(
-                      fontSize: 18,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
